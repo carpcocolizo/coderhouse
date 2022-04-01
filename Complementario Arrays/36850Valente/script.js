be702d55cd0ccaa96199657ffeffeb02eb095ed6
carpcocolizo <germanvalente@yahoo.com>
2022-03-31 21:36:59 -0300

    Subida del complementario de arrays

--- Complementario Arrays/36850Valente/script.js
+++ Complementario Arrays/36850Valente/script.js
@@ -1,47 +1,75 @@
 /*
-El objetivo del programa es para poder calcular a cuanto deberias vender algo, para poder obterner determinada cantidad en base a una comision de la plataforma de venta
-Por ejemplo, si Mercado Libre nos cobra 12.5% y queremos obtener 100 pesos de nuestra venta, algun desprevenido en un calculo simple podria crear que el precio de venta deberia ser 112.5.
-Pero en realidad para obtener nuestros 100 pesos deberiamos ponerlo a la venta en 114.29.
-
-Utilizo una sola variable global, que es para mantenerme en un loop mientras el usuario no elija salir. Podria hacer esto sin necesitar un loop posiblmente, pero para cumplir las condiciones
-del ejercicio decidi hacerlo de esta manera.
-
-Las funciones son bastante claras, tal vez podria sub dividir la funcion de calculoComision(), pero no creo que este dificil de entender.
-
-Deje como comentario el "let envio", porque se me ocurrio preguntarle al usuario si la plataforma le cobra un envio, y dependiendo de si el comprador o el vendedor se hace cargo de este, deducirlo
-o no del calculo.
-Asi como tambien pense en incluir un calculo de coste, preguntando cuanto vale comprar el producto o los materiales para armarlo y cual es la ganancia esperada.
-Pero creo que seria algo desordenado incluir toda esta informacion en prompt()
-Por lo cual en un futuro lo podria agregar, cuando usemos el DOM y nos relacionemos con el HTML, con botones y formularios se podria incluir y tener mucho mas sentido en el codigo.
+Para cumplir la consigna de incorporar arrays se me ocurrio crear cada iteracion satisfactoria del programa como un objeto y almacenarlo en una Array 
+Despues reestructure el codigo para usar las propiedad y metodos de las array y hash.
+Podria seguir mejorando el codigo, pero el proximo entregable es con este mismo codigo, por lo que voy a hacerlo en ese momento
 */
+
 let adentrodelloop = true 
 
-function calculoComision() {
+const articulo = []
+
+let i = 0
+
+class Venta {
+    constructor(nombre, comision, aobtener) {
+        this.nombre = nombre
+        this.comision = comision
+        this.aobtener = aobtener
+    }
+
+    calculoComision() {
+        let resultado = this.aobtener / (100 - this.comision) * 100;
+        return resultado = Math.round((resultado + Number.EPSILON) * 100) / 100   // No hay una forma infalible para redondear en JS, esta es la mejor opcion, en google esta la explicacion.
+
+    }
+}
+
+function crearVenta() {
+    let articulo = prompt("Cual es el nombre del articulo que quiere vender?")
     let comision = parseFloat(prompt("Cual es la comision que te cobra la plataforma?"))
     let enMano = parseFloat(prompt("Cuanto queres que te quede por la venta?"))
-    // let envio = prompt()  // Para incluir en un futuro
-    let resultado
     if (comision < 0 || comision >= 100 || enMano < 0) {
         alert("Usted a ingresado un valor invalido, ingrese nuevamente")
-        return calculoComision()  // Si no uso return, el codigo sigue y me tira los "alert" siguientes.
+        return crearVenta()  // Si no uso return, el codigo sigue y me tira los "alert" siguientes.
     } 
     resultado = enMano / (100 - comision) * 100;
         if (isNaN(resultado)) {
         alert("Usted a ingresado un valor invalido, ingrese nuevamente")
-        calculoComision()
+        return crearVenta()
     } else {
-        resultado = Math.round((resultado + Number.EPSILON) * 100) / 100   // No hay una forma infalible para redondear en JS, esta es la mejor opcion, en google esta la explicacion.
-        alert("El precio al cual debe vender para que le queden " + enMano + " en mano, es de: " + resultado)
+        return new Venta(articulo, comision, enMano)
+    }
+}
+
+function pasodos() {
+  alert("Usted a creado el siguiente articulo: " + articulo[i].nombre + "\nCon una comision del " + articulo[i].comision + "% " + "\nEl cual debe vender a: " + articulo[i].calculoComision() + "\nPara obtener " + articulo[i].aobtener)
+}
+
+function verproductos() {
+    let respuesta
+    respuesta = prompt("Desea ver los detalles de todos los productos agregados? \n Use Si o No como respuesta").toLowerCase()
+    if (respuesta == "si") {
+        for (const element of articulo) {
+            alert("Nombre de producto: " + element.nombre + "\nComision que me cobran sobre el producto " + element.comision + "%" + "\nCuanto quiero obtener: " + element.aobtener + "\nA cuanto debo venderlo: " + element.calculoComision())
+        }
+        return preguntar()
+    } else if (respuesta == "no") {
+        adentrodelloop = false
+        return alert("Gracias, vuelva prontos")
+    } else {
+        alert("Respuesta Invalida");
+        play()
     }
 }
 
 function preguntar() {
     respuesta = prompt("Quiere calcular el precio a vender en base a la comision que le cobran? \nUsar una de las siguiente respuestas: \nSi \nNo").toLowerCase()
     if (respuesta == "si") {
-        calculoComision()
+        articulo.push(crearVenta())
+        pasodos()
+        i++
     } else if (respuesta == "no") {
-        adentrodelloop = false
-        return alert("Gracias, vuelva prontos")
+        verproductos()
     } else {
         alert("Respuesta Invalida");
         play()
